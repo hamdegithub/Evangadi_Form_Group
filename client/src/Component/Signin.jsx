688ebd9a48bc74ab7e73b314e9adfddf8ed4599e
5237-mests <99667573+5237-mests@users.noreply.github.com>
2022-09-06 19:08:25 +0300

    sign in fixed

--- client/src/Component/Signin.jsx
+++ client/src/Component/Signin.jsx
@@ -2,18 +2,19 @@ import React from 'react'
 import './Signin.css'
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 
-function Signin() {
+function Signin () {
   return (
     <div className='container clcl  py-5 px-4'>
-        <h4>Login to your account</h4>
-        <p>Don't have an account?<a href='#'>Create a new account</a></p>
-        <form>
-            <input type="text" placeholder="Email address"></input><br/><br/>
-            <input type="text" placeholder="Password"></input><br/><br/>
-            <p className='forget'>Forgot password?</p>
-            <button className='signbtn'>Log In</button><br/>
-            
-        </form>
+      <h5>Login to your account</h5>
+      <p>Don't have an account?<a href='#'>Create a new account</a></p>
+      <form>
+        <input type="text" placeholder="Email address"></input><br /><br />
+        <input type="text" placeholder="Password"></input><br /><br />
+        {/* <p className='forget'>Forgot password?</p> */}
+        <button className='signbtn'>Sign In</button><br />
+        <p><a href='#'>Create a new account</a></p>
+
+      </form>
 
     </div>
   )
